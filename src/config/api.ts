a866aeb8cfd3eb044408e741bb7d8c0638a76e7c
Danilo <danilomartins.pacs@gmail.com>
2022-05-21 22:01:55 -0300

    fix

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const TIMEOUT = 1000;
 
 const instance = axios.create({
-  baseURL: 'http://192.168.3.15:3339/api/v1',
+  baseURL: process.env.API_URL,
   timeout: TIMEOUT,
 });
 
